938668de7a1de5924df86d9148fcab31c2bab825
jessicamelise <61717333+jessicamelise@users.noreply.github.com>
2020-03-03 08:51:54 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var celsiusValue = parseInt (prompt ("Digite aqui o valor em °C"));
+var convertToFahrenheit = (celsiusValue * 1.8) + 32;
+console.log("A temperatura em Fahrenheit é: " + convertToFahrenheit);
+document.write("A temperatura em Fahrenheit é: " + convertToFahrenheit);
